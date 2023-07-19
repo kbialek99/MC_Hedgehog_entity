d2a7d81176457bb1cd8090f80e2b50580cb7b899
Krzysztof <65894695+Awsikee@users.noreply.github.com>
2023-07-19 18:00:12 +0200

    added hedgehog spawn conditions

--- src/main/java/net/awsikee/mcbangermod/event/ModEventBusEvents.java
+++ src/main/java/net/awsikee/mcbangermod/event/ModEventBusEvents.java
@@ -3,7 +3,11 @@ package net.awsikee.mcbangermod.event;
 import net.awsikee.mcbangermod.McBangerMod;
 import net.awsikee.mcbangermod.entity.ModEntityTypes;
 import net.awsikee.mcbangermod.entity.custom.HedgehogEntity;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,5 +16,10 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(ModEntityTypes.HEDGEHOG.get(), HedgehogEntity.setAttributes());
+    }
+    @SubscribeEvent
+    public static void entitySpawnRestriction(SpawnPlacementRegisterEvent event) {
+       event.register(ModEntityTypes.HEDGEHOG.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+               Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
